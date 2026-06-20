8ed39c53f82f2ae1af75d2ccc6adc1c4781d2587
Tani-09 <taniya.sawlani09@gmail.com>
2026-06-20 16:32:25 +0530

    feat: implement transaction API endpoints and database migrations

--- PersonalFinance.Infrastructure/DependencyInjection/DependencyInjection.cs
+++ PersonalFinance.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using PersonalFinance.Application.Interfaces;
 using PersonalFinance.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using PersonalFinance.Infrastructure.Data;
 
 namespace PersonalFinance.Infrastructure.DependencyInjection;
 
@@ -9,6 +11,10 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(
         this IServiceCollection services)
     {
+        services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        "Server=DESKTOP-P6FRBTF,1433;Database=PersonalFinanceDb;Trusted_Connection=True;TrustServerCertificate=True;"));
+
         services.AddScoped<ITransactionRepository, TransactionRepository>();
 
         services.AddScoped<IUserRepository, UserRepository>();
